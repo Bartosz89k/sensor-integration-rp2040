c67766546bf90fea39ecc1fad7745fa0b90544d4
Bartek <bartosz89.k@gmail.com>
2024-12-23 02:10:25 +0100

    test additional led

--- src/main.c
+++ src/main.c
@@ -17,11 +17,32 @@ void led_task()
     }
 }
 
+void led_red_task()
+{
+    const uint LED_PIN = 16;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        if (temperature_value > temperature_threshold) {
+            gpio_put(LED_PIN, 1);
+            vTaskDelay(10);
+        }
+        else {
+
+            gpio_put(LED_PIN, 1);
+            vTaskDelay(10);
+            gpio_put(LED_PIN, 0);
+            vTaskDelay(10);
+        }
+    }
+}
+
 int main()
 {
     stdio_init_all();
 
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(led_red_task, "LED_Red_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
